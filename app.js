8d23737aff43945a5201d7a783f78300bcecb43b
thuanthiendo <thuanthiendola2@gmail.com>
2026-02-04 21:04:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-ldnW85PPEL3Y4SAbWEotRvmTLtzgq8o",
+  apiKey: "AIzaSyB-ldnW85PPEL3Y4SAbWEotRvmTLtzg8o",
   authDomain: "task-75413.firebaseapp.com",
   projectId: "task-75413",
-  storageBucket: "task-75413.appspot.com",
+  storageBucket: "task-75413.firebasestorage.app",
   messagingSenderId: "934934617374",
   appId: "1:934934617374:web:71ed6700a713351a72fd0f"
 };
@@ -15,26 +16,24 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const form = document.getElementById("loginForm");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const uid = userCredential.user.uid;
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    const uid = res.user.uid;
 
-    const snap = await getDoc(doc(db, "users", uid));
+    // 🔥 LẤY USER THEO UID
+    const userRef = doc(db, "users", uid);
+    const userSnap = await getDoc(userRef);
 
-    if (!snap.exists()) {
+    if (!userSnap.exists()) {
       alert("Không tìm thấy user trong database");
       return;
     }
 
-    const role = snap.data().role;
+    const role = userSnap.data().role;
 
     if (role === "admin") {
       window.location.href = "admin.html";
@@ -43,6 +42,7 @@ form.addEventListener("submit", async (e) => {
     }
 
   } catch (err) {
-    alert("Đăng nhập thất bại: " + err.message);
+    alert(err.message);
   }
-});
+};
+</script>
