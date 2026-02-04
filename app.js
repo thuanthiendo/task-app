d94a89e6ff598a931f11f39db0d9a786acbef111
thuanthiendo <thuanthiendola2@gmail.com>
2026-02-04 22:08:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT"
+  apiKey: "AIzaSyB-ldnW85PPEL3Y4SAbWEotRvmTLtzgq8o",
+  authDomain: "task-75413.firebaseapp.com",
+  projectId: "task-75413",
+  storageBucket: "task-75413.appspot.com",
+  messagingSenderId: "934934617374",
+  appId: "1:934934617374:web:71ed6700a713351a72fd0f"
 };
 
 const app = initializeApp(firebaseConfig);
