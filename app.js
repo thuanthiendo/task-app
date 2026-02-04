1dc7c26bdda8399c9d3750c1ee84a1add035d689
thuanthiendo <thuanthiendola2@gmail.com>
2026-02-04 20:31:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 console.log("APP JS ĐÃ LOAD");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-ldnw85PPELY4SAbWEotRvmTLtzq8o",
+  apiKey: "AIzaSyB-ldnw85PPELY4SAbWEt0vmTLtzqg8o",
   authDomain: "task-75413.firebaseapp.com",
   projectId: "task-75413",
+  storageBucket: "task-75413.appspot.com",
+  messagingSenderId: "934934617374",
+  appId: "1:934934617374:web:71ed6700a713351a72fd0f"
 };
 
+// Init
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
