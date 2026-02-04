5224832789c2bd332f532cd629d01d1c01b56465
thuanthiendo <thuanthiendola2@gmail.com>
2026-02-04 20:51:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-console.log("APP JS ĐÃ LOAD");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-ldnw85PPELY4SAbWEt0vmTLtzqg8o",
+  apiKey: "AIzaSyB-ldnW85PPEL3Y4SAbWEotRvmTLtzgq8o",
   authDomain: "task-75413.firebaseapp.com",
   projectId: "task-75413",
   storageBucket: "task-75413.appspot.com",
@@ -9,32 +11,38 @@ const firebaseConfig = {
   appId: "1:934934617374:web:71ed6700a713351a72fd0f"
 };
 
-// Init
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+const form = document.getElementById("loginForm");
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-function login() {
-  if (!email.value || !password.value) {
-    alert("Nhập email và mật khẩu");
-    return;
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const uid = userCredential.user.uid;
+
+    const snap = await getDoc(doc(db, "users", uid));
+
+    if (!snap.exists()) {
+      alert("Không tìm thấy user trong database");
+      return;
+    }
+
+    const role = snap.data().role;
+
+    if (role === "admin") {
+      window.location.href = "admin.html";
+    } else {
+      window.location.href = "employee.html";
+    }
+
+  } catch (err) {
+    alert("Đăng nhập thất bại: " + err.message);
   }
-
-  auth.signInWithEmailAndPassword(email.value, password.value)
-    .then(u => {
-      if (u.user.email.includes("admin")) {
-        location.href = "admin.html";
-      } else {
-        location.href = "employee.html";
-      }
-    })
-    .catch(err => alert(err.message));
-}
-auth.onAuthStateChanged(user => {
-  console.log("Auth state:", user);
 });
